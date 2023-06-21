66e7246f03bf22cb2c9190ffd01c074398ebd5d6
bashar <bashar29@hotmail.fr>
2023-06-21 22:36:36 +0200

    Yew callback

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -28,22 +28,27 @@ fn app() -> Html {
             _url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
     ];
-    // let videos = videos.iter().map(|video| html! {
-    //     <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
-    // }).collect::<Html>();
+
+    let selected_video = use_state(|| None);
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| {
+            selected_video.set(Some(video))
+        })
+    };
+    
+    let details = selected_video.as_ref().map(|video| html! {
+        <VideoDetails video={video.clone()} />
+    });
 
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                //{ videos }
-                <VideosList videos={videos} />
+                <VideosList videos={videos} on_click={on_video_select.clone()} />
             </div>
-            <div>
-                <h3>{ "John Doe: Building and breaking things" }</h3>
-                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
-            </div>
+            { for details }
         </>
     }
 }
@@ -63,15 +68,40 @@ struct Video {
 #[derive(Properties, PartialEq)]
 struct VideosListProps {
     videos: Vec<Video>,
+    on_click: Callback<Video>,
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    }
 }
 
 #[function_component(VideosList)]
-fn videos_list(VideosListProps { videos }: &VideosListProps) -> Html {
+fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+    let on_click = on_click.clone();
     videos
         .iter()
         .map(|video| {
+            let on_video_select = {
+                let on_click = on_click.clone();
+                let video = video.clone();
+                Callback::from(move |_| {
+                    on_click.emit(video.clone())
+                })
+            };
             html! {
-                <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+                //<p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+                <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
             }
         })
         .collect()
