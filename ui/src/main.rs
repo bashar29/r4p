2bd87c217bc379ce23e981b1e9c9137702b76370
bashar <bashar29@hotmail.fr>
2023-06-21 22:07:11 +0200

    Yew Component

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -7,37 +7,38 @@ fn app() -> Html {
             id: 1,
             title: "Building and breaking things".to_string(),
             speaker: "John Doe".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+            _url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
         Video {
             id: 2,
             title: "The development process".to_string(),
             speaker: "Jane Smith".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+            _url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
         Video {
             id: 3,
             title: "The Web 7.0.0".to_string(),
             speaker: "Matt Miller".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+            _url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
         Video {
             id: 4,
             title: "Mouseless development".to_string(),
             speaker: "Tom Jerry".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+            _url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
     ];
-    let videos = videos.iter().map(|video| html! {
-        <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
-    }).collect::<Html>();
+    // let videos = videos.iter().map(|video| html! {
+    //     <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+    // }).collect::<Html>();
 
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                { videos }
+                //{ videos }
+                <VideosList videos={videos} />
             </div>
             <div>
                 <h3>{ "John Doe: Building and breaking things" }</h3>
@@ -51,9 +52,27 @@ fn main() {
     yew::Renderer::<MyApp>::new().render();
 }
 
+#[derive(Clone, PartialEq)]
 struct Video {
     id: usize,
     title: String,
     speaker: String,
-    url: String,
+    _url: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos }: &VideosListProps) -> Html {
+    videos
+        .iter()
+        .map(|video| {
+            html! {
+                <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+            }
+        })
+        .collect()
 }
