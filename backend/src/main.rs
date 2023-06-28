c0fa5512873dba3548ea1454799ca10aab6a81c0
bashar <bashar29@hotmail.fr>
2023-06-28 21:36:39 +0200

    configuration management

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,11 @@
+use backend::configuration::get_configuration;
 use backend::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    let settings = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", settings.application_port);
+
+    let listener = std::net::TcpListener::bind(address)?;
     run(listener)?.await
 }
