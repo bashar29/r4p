5c700833788954f75b8864f06bdd82f8d231379a
bashar <bashar29@hotmail.fr>
2023-07-13 15:47:20 +0200

    preparation of tests

--- backend/tests/api/account.rs
+++ backend/tests/api/account.rs
@@ -1,1 +1,22 @@
+use crate::helpers;
 
+#[actix_web::test]
+async fn create_account_return_200_for_valid_data() {
+    // Arrange
+    let test_app = helpers::spawn_app().await;
+    let creation_body = serde_json::json!({
+        "company_name" : "The company corp.",
+        "admin_lastname" : "Johnson",
+        "admin_firstname" : "Bill"
+    });
+
+    // Act
+    let response = test_app.post_account(&creation_body).await;
+
+    // Assert
+    // assert_eq!(200, response.status().as_u16());
+}
+
+async fn create_account_persist_the_new_account() {}
+
+async fn create_account_create_admin_user_for_the_account() {}
--- backend/tests/api/helpers.rs
+++ backend/tests/api/helpers.rs
@@ -5,6 +5,7 @@ use std::net::TcpListener;
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub api_client: reqwest::Client,
 }
 
 pub async fn spawn_app() -> TestApp {
@@ -19,8 +20,29 @@ pub async fn spawn_app() -> TestApp {
     let server =
         backend::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap();
+
     TestApp {
         address: format!("http://127.0.0.1:{}", port),
         db_pool: connection_pool,
+        api_client: client,
+    }
+}
+
+impl TestApp {
+    pub async fn post_account<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
+        self.api_client
+            .post(&format!("{}/accounts", &self.address))
+            .form(body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
     }
 }
